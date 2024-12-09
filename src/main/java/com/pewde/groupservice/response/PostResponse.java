949af551a9f32b1a6e1aac7b3c8b75caac2d4f74
e.levin <e.levin@solarlab.ru>
2024-12-09 14:47:17 +0300

    Убрал неиспользуемый импорт :)

--- src/main/java/com/pewde/groupservice/response/PostResponse.java
+++ src/main/java/com/pewde/groupservice/response/PostResponse.java
@@ -5,7 +5,6 @@ import com.pewde.groupservice.entity.Wall;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.http.HttpStatus;
 
 import java.util.Date;
 
